654d12d00a0c7ba1337549b3f02982f2b418d8b0
datngotien345 <datngotien345@gmail.com>
2024-10-13 10:09:52 +0700

    update post

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -2,8 +2,14 @@ import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "../dtos/create-user-dto";
 import { GetUsersParamDto } from "../dtos/get-users-param.dto";
 
+/**
+ * Class to connect to Users table and perform business operations
+ */
 @Injectable()
 export class UserService {
+    /**
+   * The method to get all the users from the database
+   */
     public findAll(
         getUserParamDto: GetUsersParamDto,
         limit: number,
@@ -21,6 +27,9 @@ export class UserService {
         ]
     }
 
+     /**
+     * Find a single user using the ID of the user
+     */
     public findOneById(id: number) {
         return {
             id: 1234,
