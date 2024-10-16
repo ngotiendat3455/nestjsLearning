dbed8dd4d13fbbe3b42d795a15ad2b567091def8
ngo tien dat <datngotien345@gmail.com>
2024-10-16 21:15:58 +0700

    implement hashing provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { HashingProvider } from './providers/hashing.provider';
+import { BcryptProvider } from './providers/bcrypt.provider';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, HashingProvider, BcryptProvider],
   controllers: [AuthController],
   exports: [AuthService],
   imports: [forwardRef(() => UsersModule)],
---
+++ src/auth/providers/bcrypt.provider.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { HashingProvider } from './hashing.provider';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class BcryptProvider extends HashingProvider{
+    async comparePassword(data: string | Buffer, encrypted: string): Promise<boolean> {
+        return bcrypt.compare(data, encrypted);
+    }
+    async hashPassword(data: string | Buffer): Promise<string> {
+        // Generate the salt
+        const salt = await bcrypt.genSalt();
+        return bcrypt.hash(data, salt);
+    }
+}
