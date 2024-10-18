2bd55556892ed1ac681116b636dac1f669494d05
ngotiendat3455 <datngotien345@gmail.com>
2024-10-19 00:11:25 +0700

    apply decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignInDto } from './dto/singin.dto';
 import { AuthService } from './providers/auth.service';
+import { Auth } from './decorators/auth.decorator';
+import { AuthType } from './enum/auth-type.enum';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService){}
     @Post('sign-in')
+    @Auth(AuthType.None)
     public async signIn(@Body() signInDto: SignInDto) {
         return this.authService.signIn(signInDto);
     }
