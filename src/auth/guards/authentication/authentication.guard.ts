e26466eb094e05eaf52e4cc6473b75065013df99
ngotiendat3455 <datngotien345@gmail.com>
2024-10-19 10:43:46 +0700

    update authentication guard

--- src/auth/guards/authentication/authentication.guard.ts
+++ src/auth/guards/authentication/authentication.guard.ts
@@ -1,8 +1,9 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
 import { AuthType } from "src/auth/enum/auth-type.enum";
 import { AccessTokenGuard } from "../access-token/access-token.guard";
+import { AUTH_TYPE_KEY } from "src/auth/decorators/auth.decorator";
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
@@ -12,17 +13,42 @@ export class AuthenticationGuard implements CanActivate {
     constructor(
         private readonly reflector: Reflector,
         private readonly accessTokenGuard: AccessTokenGuard,
-      ) {}
+    ) { }
     // create authTypeGuardMap
     private readonly authTypeGuardMap: Record<AuthType, CanActivate | CanActivate[]> = {
         [AuthType.Bearer]: this.accessTokenGuard,
         [AuthType.None]: { canActivate: () => true }
     }
 
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
         console.log(this.authTypeGuardMap);
-        return true;
+        // Print authTypeGuardMap
+        const authTypes = this.reflector.getAllAndOverride<AuthType[]>(
+            AUTH_TYPE_KEY,
+            [context.getHandler(), context.getClass()],
+        ) ?? [AuthenticationGuard.defaultAuthType];
+        // Show what are authTypes
+        console.log(authTypes);
+        const guards = authTypes.map((type) => this.authTypeGuardMap[type]).flat();
+        // Declare the default error
+        let error = new UnauthorizedException();
+
+        for (let instance of guards) {
+            console.log(instance);
+            const canActivate = await Promise.resolve(
+                instance.canActivate(context),
+            ).catch((err) => {
+                error = err;
+            });
+
+            // Display Can Activate
+            console.log(canActivate);
+            if (canActivate) {
+                return true;
+            }
+        }
+        //return true;
     }
 
-    
+
 }
