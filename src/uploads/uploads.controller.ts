8b9db7f552b70e6ab19936f24b13f79c287a5142
datngotien345 <datngotien345@gmail.com>
2024-10-20 14:47:55 +0700

    test upload file

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,24 +1,35 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    UploadedFile,
+    UseInterceptors,
+  } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiHeader, ApiHeaders, ApiOperation } from '@nestjs/swagger';
 import { UploadsService } from './providers/uploads.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { AuthType } from 'src/auth/enum/auth-type.enum';
+import { diskStorage } from 'multer';
 
+@Auth(AuthType.None)
 @Controller('uploads')
 export class UploadsController {
 
     constructor(
         private readonly uploadService: UploadsService
     ){}
+
     @UseInterceptors(FileInterceptor('file'))
-    @Post('file')
     @ApiHeaders([
         { name: 'Content-Type', description: 'multipart/form-data' },
         { name: 'Authorization', description: 'Bearer Token' },
-      ])
+    ])
     @ApiOperation({
         summary: `Upload a new image to the server`,
-      })
+    })
+    @Post('file')
     public uploadFile(@UploadedFile() file: Express.Multer.File){
+        console.log(file); // Handle file here
         return this.uploadService.uploadFile(file);
     }
 }
