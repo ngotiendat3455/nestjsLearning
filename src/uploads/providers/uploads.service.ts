866924352e243fd1c79e438b04cd9358ea6c3df7
datngotien345 <datngotien345@gmail.com>
2024-10-20 14:23:14 +0700

    finish upload file

--- src/uploads/providers/uploads.service.ts
+++ src/uploads/providers/uploads.service.ts
@@ -1,20 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
 import { UploadToAwsProvider } from './upload-to-aws.provider';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Upload } from '../upload.entity';
 import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { UploadFile } from '../interfaces/upload-file.interface';
+import { fileTypes } from '../enums/file-types.enum';
 
 @Injectable()
 export class UploadsService {
     constructor(
         private readonly uploadToAwsProvider: UploadToAwsProvider,
+        private readonly configService: ConfigService,
         @InjectRepository(Upload)
-        private usersRepository: Repository<Upload>,
+        private uploadsRepository: Repository<Upload>,
     ){}
 
     public async uploadFile(file: Express.Multer.File) {
-        // upload to the file to AWS s3
-        const name = await this.uploadToAwsProvider.fileupload(file);
-        // generate to a new  entry in database
+        // throw error for unsupported MIME types
+        if (
+            !['image/gif', 'image/jpeg', 'image/jpg', 'image/png'].includes(
+              file.mimetype,
+            )
+          ) {
+            throw new BadRequestException('MIME type not supported');
+          }
+        try{
+            // upload to the file to AWS s3
+            const name = await this.uploadToAwsProvider.fileupload(file);
+            // generate to a new  entry in database
+            const uploadObj: UploadFile = {
+                name: name,
+                path: `https://${this.configService.get<string>('appConfig.awsCloudfrontUrl')}/${name}`,
+                type: fileTypes.IMAGE,
+                mime: file.mimetype,
+                size: file.size,
+            }
+
+            // create an upload
+            const upload = this.uploadsRepository.create(uploadObj);
+            // save the details to database
+            return await this.uploadsRepository.save(upload);
+        } catch (error) {
+            throw new ConflictException(error);
+          }
     }
 }
